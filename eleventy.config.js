58db7749c4483cf8ff65bfe02d62b335ae21f1cd
Øystein Håberg <islandstone89@gmail.com>
2025-04-01 21:52:26 +0200

    * Added paired shortcode in config file

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,11 @@
 export default function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/css");
 
+  eleventyConfig.addPairedShortcode('element', (content, el = 'aside', className) => {
+      return `<${el} class="${className}">${content}</${el}>`
+    }
+  )
+
   return {  
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
