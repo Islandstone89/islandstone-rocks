fc75452a48a0772b54dc579cdc085ac74793089e
Øystein Håberg <islandstone89@gmail.com>
2025-05-08 23:59:10 +0200

    * Managed to render Montserrat font * Experimented with bg color * Added box-shadow on team cards * Copied content from test.html to home.html to see how it looks in page container

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,8 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/css");
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
-  eleventyConfig.addPassthroughCopy("/assets/fonts/montserrat-v29-latin-regular.woff2");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts/montserrat-v29-latin-regular.woff2");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts/montserrat-v29-latin-700.woff2");
 
   eleventyConfig.addPairedShortcode('element', (content, el = 'aside', className) => {
       return `<${el} class="${className}">${content}</${el}>`
