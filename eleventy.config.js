45c4bf2249386c907f23300e23b921850e9141f4
Øystein Håberg <islandstone89@gmail.com>
2025-05-11 00:16:30 +0200

    * Added more fonts * Updated config file to passthrough every "woff2" file * Removed bg color header

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,8 +3,7 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/css");
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
-  eleventyConfig.addPassthroughCopy("src/assets/fonts/montserrat-v29-latin-regular.woff2");
-  eleventyConfig.addPassthroughCopy("src/assets/fonts/montserrat-v29-latin-700.woff2");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts/*.woff2");
 
   eleventyConfig.addPairedShortcode('element', (content, el = 'aside', className) => {
       return `<${el} class="${className}">${content}</${el}>`
